8d36500e8e69b4bb14ebad529604489eab81b863
istioarm64 <97154425+istioarm64@users.noreply.github.com>
2022-02-21 16:29:32 +0800

    Update lib.sh

--- prow/lib.sh
+++ prow/lib.sh
@@ -119,12 +119,20 @@ function build_images() {
 
   # use ubuntu:bionic to test vms by default
   nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_bionic "
-  if [[ "${SELECT_TEST}" == "test.integration.pilot.kube" ]]; then
+    
+  # For arm64, only run docker.app, but bypass all others
+  if [[ "$(uname -m)" == "aarch64" ]]; then
+     echo "this is aarch64 platform, choose docker.app"
+     nonDistrolessTargets="docker.app "
+  elif [[ "${SELECT_TEST}" == "test.integration.pilot.kube" &&  ]]; then
+    #  if [[ "${SELECT_TEST}" == "test.integration.pilot.kube" &&  ]]; then
     nonDistrolessTargets+="docker.app_sidecar_ubuntu_xenial docker.app_sidecar_ubuntu_focal docker.app_sidecar_ubuntu_bionic "
     nonDistrolessTargets+="docker.app_sidecar_debian_9 docker.app_sidecar_debian_10 docker.app_sidecar_centos_7 docker.app_sidecar_centos_8 "
   fi
+
   targets+="docker.operator "
   targets+="docker.install-cni "
+  
   printdir=`ls -la ~/`
   echo "lib.sh: ttttttttttttt:"
   echo $printdir
